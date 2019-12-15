01788279caa9d5db5cc084c94b048aec7ec9527c
Stanislav Vlasov <vvs00794@gmail.com>
2019-12-15 16:14:15 +0400

    choose sity

--- js/script.js
+++ js/script.js
@@ -1,28 +1,37 @@
 'use strict'
 
 window.addEventListener('DOMContentLoaded', function () {
-
     const temperature = document.querySelector('.temperature');
     const descriptionWeather = document.querySelector('.wearher_discription');
     const city = document.querySelector('.city');
     const cities = document.querySelector('#cities');
+    const chooseCity = document.querySelector('.cities');
+    const formCity = document.querySelector('form');
+    let cityId = 1496153; // Omsk id
 
-    let cityId = 1496153;
     getTemperature();
+    getCity();
 
-    cities.addEventListener('click', event => {
+    chooseCity.addEventListener('click', () => {
+        formCity.classList.toggle('city_form');
+    });
+
+    cities.addEventListener('click', () => {
         cityId = cities.value;
         getTemperature();
     });
 
     async function getTemperature() {
-        
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=6d69f33007962728c2d73296d6bbdd69`);
         let result = await response.json();
-
-        let { temp } = result.main;
+        let {
+            temp
+        } = result.main;
         let weather = result.weather[0];
-        let {description, main } = weather;
+        let {
+            description,
+            main
+        } = weather;
         const celsius = Math.floor(temp - 273.15);
 
         skycons(main);
@@ -31,6 +40,22 @@ window.addEventListener('DOMContentLoaded', function () {
         descriptionWeather.innerHTML = description;
         city.innerHTML = result.name;
     };
+
+    async function getCity() {
+        let response = await fetch('../json/city.list.json');
+        let data = await response.json();
+
+        for (let key in data) {
+            if (data[key].country === 'RU') {
+                let option = new Option(data[key].name, data[key].id);
+                cities.append(option);
+                if (data[key].id === 1496153) {
+                    cities.value = 1496153;
+                }
+            }
+        }
+    };
+
 
 
     function skycons(fallout) {
@@ -58,26 +83,10 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         if (fallout === 'Fog') {
             skycons.add("icon1", Skycons.FOG);
-        }
-
-        else {
+        } else {
             skycons.add("icon1", Skycons.CLEAR_DAY);
         }
 
         skycons.play();
     }
-
-    async function getCity(){
-        let response = await fetch('../json/city.list.json');
-        let data = await response.json();
-
-        for(let key in data){
-            if(data[key].country === 'RU'){
-                let option =  new Option( data[key].name, data[key].id);
-                cities.append(option);
-            }
-        }
-    };
-
-    getCity();
 });
